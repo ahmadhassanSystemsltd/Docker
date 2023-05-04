e62c9d751599523db6a21e3ce5a85202bed5e249
ahmadhassanSystemsltd <109286114+ahmadhassanSystemsltd@users.noreply.github.com>
2023-05-04 23:40:02 +0500

    Update docker-setup.sh

--- docker-setup.sh
+++ docker-setup.sh
@@ -1,12 +1,12 @@
-
 sudo yum install -y yum-utils 
 sudo yum-config-manager --add-repo   https://download.docker.com/linux/centos/docker-ce.repo 
-sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo yum -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo systemctl status docker
-sudo usermod -aG docker centos 
+sudo usermod -aG docker username 
 sudo systemctl restart docker 
 To run the below command wait for 5 Sec 
+sleep(5)
 sudo chmod 666 /var/run/docker.sock 
 sudo systemctl restart docker 
